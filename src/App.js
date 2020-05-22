c109726ff0b686fafc261a5b451c468bb91f9db8
Grisel Matos <griselmatosm@gmail.com>
2020-05-22 10:04:52 +0200

    Add all files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SeasonDisplay from './components/SeasonDisplay';
+import Spinner from './components/Spinner';
+
+class App extends React.Component {
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }), //success callback
+      (err) => this.setState({ errorMessage: err.message }) // error callback
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+export default App;
